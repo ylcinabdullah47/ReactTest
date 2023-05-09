e736b8e4ebd5c92496f78e3e8011c7e7b229956c
abdullah <a.ylcin47@gmail.com>
2023-05-09 15:18:28 +0300

    router ile ilgili işlemlerr yapıldı

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import Deneme from './components/deneme.jsx';
 import Deneme2 from './components/deneme2.jsx';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { AiFillCopyrightCircle } from 'react-icons/ai'
+import { Routes } from 'react-router';
+import Home from './pages/Home.jsx';
+import Test from './pages/Test.jsx';
 const names = ["Ali", "Veli", "Ayşe", "Fatma"];// başka kaynaktan alındı map kullanımı ile ilgili
 function App() {
   // HATALI ÇALIŞIYOR
@@ -43,55 +48,55 @@ function App() {
 
 
   //hook kullanımı 
-  const [count, setCount] = useState(0)
-  console.log("count", count);
-  const firstRef = useRef();
+  // const [count, setCount] = useState(0)
+  // console.log("count", count);
+  // const firstRef = useRef();
 
   //USEMEMO KONUSUNU TEKRAR ET
 
-  const [count33, setCount33] = useState(0);
-  const [text33, setText33] = useState("")
+  // const [count33, setCount33] = useState(0);
+  // const [text33, setText33] = useState("")
 
-  const func = (num) => {
-    console.log("hesaplanıyor....");
-    for (let i = 0; i < 25000; i++) {
-      num += 1
+  // const func = (num) => {
+  //   console.log("hesaplanıyor....");
+  //   for (let i = 0; i < 25000; i++) {
+  //     num += 1
 
-    }
-    return num
-  }
+  //   }
+  //   return num
+  // }
 
-  const memo = useMemo(() => func(count33), [text33])
+  // const memo = useMemo(() => func(count33), [text33])
 
 
 
 
   //useefect bu konuya tekrar bakılacak
-  const [test, günceltest] = useState("deneme")
-  useEffect(() => {
-    günceltest("deneme güncelleidni")
-  }, [])
+  // const [test, günceltest] = useState("deneme")
+  // useEffect(() => {
+  //   günceltest("deneme güncelleidni")
+  // }, [])
 
 
   //HOOK KULLANIMI ÖRNEK 2
 
-  const [count47, setCount47] = useState(0);
+  // const [count47, setCount47] = useState(0);
 
-  useEffect(() => {
-    console.log("useEffect called");
-  }, []);
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // }, []);
 
-  console.log("component TEST2");
+  // console.log("component TEST2");
 
-  //HOOK KUALLANIMI ÖRNEK 3
-  const [count123, setCount123] = useState(0);
-  const [text, setText] = useState("");
+  // //HOOK KUALLANIMI ÖRNEK 3
+  // const [count123, setCount123] = useState(0);
+  // const [text, setText] = useState("");
 
-  useEffect(() => {
-    console.log("useEffect called");
-  }, [count, text]);
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // }, [count, text]);
 
-  console.log("component TEST3");
+  // console.log("component TEST3");
 
 
 
@@ -99,56 +104,73 @@ function App() {
 
 
   //map kullanımı ile ilgili başka kaynaktan örnek
-  const renderListOfNames = (names) => {
-    return names.map((name) => <li>{name}</li>);
-  };
-  const arr = [{ name: 'array1', id: 0 }, { name: 'array2', id: 1 }, { name: 'array3', id: 2 }]
+  // const renderListOfNames = (names) => {
+  //   return names.map((name) => <li>{name}</li>);
+  // };
+  // const arr = [{ name: 'array1', id: 0 }, { name: 'array2', id: 1 }, { name: 'array3', id: 2 }]
 
 
-  console.log('array:', arr);
-  const test47 = "selam ben adbullah 47"
-  const clickFunck = () => {
-    console.log("butona tıkladın");
-    setCount(count + 15)
-  }
+  // console.log('array:', arr);
+  // const test47 = "selam ben adbullah 47"
+  // const clickFunck = () => {
+  //   console.log("butona tıkladın");
+  //   setCount(count + 15)
+  // }
   return (
-    <div className="App">
-      <div>test</div>
-      <Sayfa47 abdullah={test47} />
-      <Deneme />
-      <Deneme2 />
-      <div>{test}</div>
-      {/* //map kullanımı ile ilgili başka kaynaktan örnek */}
-      <div>
-        <ul >{renderListOfNames(names)}</ul>
-      </div>
-      {
-        arr.map((ar) => {
-          <div key={ar.id}>{ar.name}</div>
-        })
-      }
-      {count}
+    //     <div className="App">
+    //       <div>test</div>
+    //       <Sayfa47 abdullah={test47} />
+    //       <Deneme />
+    //       <Deneme2 />
+    //       <div>{test}</div>
+    //       {/* //map kullanımı ile ilgili başka kaynaktan örnek */}
+    //       <div>
+    //         <ul >{renderListOfNames(names)}</ul>
+    //       </div>
+    //       {
+    //         arr.map((ar) => {
+    //           <div key={ar.id}>{ar.name}</div>
+    //         })
+    //       }
+    //       {count}
 
-      <button onClick={clickFunck}> tıkla</button>
+    //       <button onClick={clickFunck}> tıkla</button>
 
-      {/* <div>{status} </div> */}
-      <input ref={firstRef} placeholder='ara' />
-      <div>
-        <p>Count: {count47}</p>
-        <button onClick={() => setCount47(count47 + 1)}>TEST</button>
-      </div>
+    //       {/* <div>{status} </div> */}
+    //       <input ref={firstRef} placeholder='ara' />
+    //       <div>
+    //         <p>Count: {count47}</p>
+    //         <button onClick={() => setCount47(count47 + 1)}>TEST</button>
+    //       </div>
 
-      <div>
-        <p>Count: {count123}</p>
-        <button onClick={() => setCount123(count123 + 1)}>TEST3</button>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+    //       <div>
+    //         <p>Count: {count123}</p>
+    //         <button onClick={() => setCount123(count123 + 1)}>TEST3</button>
+    //         <input value={text} onChange={(e) => setText(e.target.value)} />
+    //       </div>
 
-      {memo}
-      <input value={text33} onchange={e => setText33(e.target.value)} placeholder='ara' />
-    </div>
+    //       {/* {memo}
+    //       <input value={text33} onchange={e => setText33(e.target.value)} placeholder='ara' /> */}
+
+    //       <  AiFillCopyrightCircle />
+
+    // {/* //router deneme1 */}
 
 
+
+
+
+    //     </div>
+
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/test' element={<Test />} />
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
